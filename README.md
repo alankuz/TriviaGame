18e20fa30c5382df6237b57816d73929d3930a79
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-08-05 17:37:43 -0600

    Added readme information, fixed a lot of the loose code, and have tightend up the formatting.

--- README.md
+++ README.md
@@ -1,21 +1,60 @@
 # TriviaGame
-psuedocode:
-press start to start the game.
 
-Question in shown, which includes a timer for 30 seconds, and displays a question as well as 4 answers.
+This project is a trivia game that asks you trivia questions about popular kids cartoons from the 2000's. You need to answer as many questions correct as possible and receive the highest correct answer score you can! Watch out, as you only have 30 seconds each questions. No using outside sources, only your own knowledge. 
+
+## Getting Started
+
+Playing is simple. Answer as many of the 2000's cartoon trivia questions correctly as you can. Once you finish answering questions you will be provided with a score and may play again. To select an answer simply click the answer you believe is correct. 
+
+### Prerequisites
+
+-Fingers
+-Functional mouse/trackpad
+-Newest browser update recommended**
+
+### Installing
+
+To install, and modify this program, you may press the fork button and then fork it to your computer through either gitbash, or terminal. To play to game you may press the following link:
+[Trivia Game](https://alankuz.github.io/TriviaGame/)
+
+## Deployment
+
+You may fork the files, or use the link to add this to your own pages.
+
+## Built With
+
+* [Jquery](https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js) - Script framework
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags). 
+
+## Author
+
+* **Alan Kuzmanovic** - *Initial work* - [alankuz](https://github.com/alankuz)
+
+## License
+
+No license agreements, do as you will with the program. 
+
+##Psuedocode:
+
+Press start to start the game.
+
+Question is shown, which includes a timer for 30 seconds, and displays a question as well as 4 answers.
 When you click an answer. it will tell you if correct or incorrect
 
 If timer runs out: shows you are out of time. Shows the correct answer, and then comes up with a new question. 
 
-If correct: Will display that you got it correct. Will then congraduate you. Has a delay. Then will record that you got it correct. and will display a new questions w/answer with a restarting timer. 
+If correct: Will display that you got it correct. Will then congratulate you. Then a delay for around 4 seconds. Then will record that you got it correct. and will display a new questions w/answer with a restarting timer. 
 
-If incorrect: will change text to display incorrect. Will then show the correct answer. For about 3 seconds. Will then add incorrect answer to an array to remember if you are incorrect. and will display a new questions w/ answers with a resarting time counting down from 30. NO KEYPRESS TO START NEXT QUESTION
+If incorrect: will change text to display incorrect. Will then show the correct answer. For about 4 seconds. Will then add incorrect answer to an array to remember if you are incorrect. and will display a new questions w/ answers with a resarting timer counting down from 30. NO KEYPRESS TO START NEXT QUESTION
 
 Once you answer last questions it will display:
 
 Correct answrs:
 incorrect answers:
 Unanswered:
-Timer is no longer ticking down//may want to remove it. 
+Timer is no longer ticking down and is removed.
 Start over button appears. Resets the game. //Same questions
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// Global Variables---------------
 var correctA = 0
 var incorrectA = 0
 var unansweredA = 0
@@ -20,55 +21,50 @@ var questions = [
 var answers = []
 var tOut;
 var trans;
-// function onload() {
-// $('#triv-container').html('<button id="startbtn">Start</button>');
-// }
-$('#startbtn').click(function () {
 
-    // answers = ['Spongebob', 'Patrick', 'Squidward', 'Sandy']
+$('#startbtn').click(function () {
     trivia();
 });
 
+// Main questions and answer loading function, this is the what creates the main content---------------------
 function trivia() {
-    console.log(x)
     clearInterval(tOut);
     clearInterval(trans);
     if (x < 11) {
-        sec =30
-        console.log('pressed')
+        sec = 30
         if (x === 0) {
             answers = ['Spongebob', 'Patrick', 'Squidward', 'Sandy']
-                    }
+        }
         if (x === 1) {
             answers = ['Pickle', 'Sparky', 'Kimchi', 'Scooby']
-                    }
+        }
         if (x === 2) {
             answers = ['6', '5', '12', '10']
-                    }
+        }
         if (x === 3) {
             answers = ['Fist', 'Hip', 'Foot', 'Knuckle']
-                    }
+        }
         if (x === 4) {
             answers = ['Monkies', 'Genies', 'Fairies', 'Scientists']
-                    }
+        }
         if (x === 5) {
             answers = ['Emily', 'Sarah', 'Nazz', 'Kacey']
-                    }
+        }
         if (x === 6) {
             answers = ['Jamaican', 'British', 'Russian', 'Finnish']
-                    }
+        }
         if (x === 7) {
             answers = ['Uno', 'Stevie', 'Charlie', 'Nigel']
-                    }
+        }
         if (x === 8) {
             answers = ['Beaver', 'Dog', 'Platypus', 'Otter']
-                    }
+        }
         if (x === 9) {
             answers = ['Kidney', 'Lentils', 'Black', 'Pinto']
-                    }
+        }
         if (x === 10) {
             answers = ['DarkLord', 'MegaLord', 'UltraLord', 'SuperLord']
-                    }
+        }
         $('#triv-container').html('<h1 id="question">' + questions[x] + '</h1><br><a onclick = "a1()"><ul class="one ques" >' + answers[0] + '</ul></a> <a onclick = "a2()"><ul class="two  ques">' + answers[1] + '</ul></a> <a onclick = "a3()"><ul class="three  ques">' + answers[2] + '</ul></a> <a onclick = "a4()"><ul class="four  ques">' + answers[3] + '</ul></a>');
         countDown()
     }
@@ -77,86 +73,93 @@ function trivia() {
         gameEnd();
     }
 }
+
 function countDown() {
-    // tOut = setInterval(timeUp, 25000)
-    $('#timer').html(sec)
-   
-        
-         time =setInterval(function () {
-            if (sec <1) {
-                clearInterval(time);
-                sec++
-                timeUp()}
-               sec--
-        $('#timer').html(sec)}, 1000);}
+ $('#timer').html(sec)
+    time = setInterval(function () {
+        if (sec < 1) {
+            clearInterval(time);
+            sec++;
+            timeUp()
+        }
 
+        sec--;
+        $('#timer').html(sec);
+    }, 1000);
+}
 
-    
-
-
+// When the clock is out of time -----------------------------------
 function timeUp() {
-    console.log('timeout')
     clearInterval(tOut);
     clearInterval(trans);
     if (x === 0 || x === 6 || x === 9) {
-        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[0] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[0] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 5) {
-        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[1] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[1] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 1 || x === 4 || x === 8 || x === 10) {
-        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[2] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[2] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 2 || x === 3 || x === 7) {
-        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[3] + '</p><img src="assets/images/' +x +'.gif">')
-    }    x++;
+        $('#triv-container').html('<h1 id="notimeorwrong">Times Up!</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[3] + '</p><img src="assets/images/' + x + '.gif">')
+    } 
+    x++;
     unansweredA++;
     setTimeout(trivia, 4000)
 }
+
+// When you get an answer correct-----------------------------------------
 function right() {
     clearInterval(time);
-                sec =30
+    sec = 30
     x++;
     correctA++;
     $('#triv-container').html('<h1 id="notimeorwrong">CORRECT!</h1><img src="assets/images/win.gif">')
     trans = setTimeout(trivia, 4000);
 
 }
+
+// When you get an answer wrong-------------------------------------
 function wrong() {
     clearInterval(time);
-                sec =30
+    sec = 30
     if (x === 0 || x === 6 || x === 9) {
-        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[0] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[0] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 5) {
-        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[1] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[1] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 1 || x === 4 || x === 8 || x === 10) {
-        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[2] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[2] + '</p><img src="assets/images/' + x + '.gif">')
     }
     if (x === 2 || x === 3 || x === 7) {
-        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[3] + '</p><img src="assets/images/' +x +'.gif">')
+        $('#triv-container').html('<h1 id="notimeorwrong">INCORRECT</h1><p class = "incorrectanswer">Correct Answer Is:</p>' + '<p class = "incorrectanswer">' + answers[3] + '</p><img src="assets/images/' + x + '.gif">')
     }
 
     x++;
     incorrectA++;
     trans = setTimeout(trivia, 4000);
 }
+
+// When the game ends -----------------------------------
 function gameEnd() {
     $('#triv-container').html('<ul class= "scores">Correct Answers: ' + correctA + '</ul><ul class= "scores">Incorrect Answers: ' + incorrectA + '</ul><ul class= "scores">Unanswered: ' + unansweredA + '</ul><a onclick = "reset()"><h1 id="end-button">Click Here to play again!</h1></a>')
     $('#timer').html('')
 }
+
+// Reset function--------------------------------
 function reset() {
     clearInterval(time)
-    x=0;
+    x = 0;
     correctA = 0;
     incorrectA = 0;
     unansweredA = 0;
     trivia();
-
 }
+
+// Answer 1 function ------------------------
 function a1() {
-    console.log('clicked');
     clearTimeout(tOut);
     if (x === 0 || x === 6 || x === 9) {
         right()
@@ -165,6 +168,8 @@ function a1() {
         wrong()
     }
 }
+
+// Answer 2 function-----------------------------
 function a2() {
     clearTimeout(tOut);
     if (x === 5) {
@@ -174,6 +179,8 @@ function a2() {
         wrong()
     }
 }
+
+// Answer 3 function-----------------------------
 function a3() {
     clearTimeout(tOut);
     if (x === 1 || x === 4 || x === 8 || x === 10) {
@@ -183,6 +190,8 @@ function a3() {
         wrong()
     }
 }
+
+// Answer 4 function-----------------------------
 function a4() {
     clearTimeout(tOut);
     if (x === 2 || x === 3 || x === 7) {
@@ -192,4 +201,3 @@ function a4() {
         wrong()
     }
 }
-// onload()
